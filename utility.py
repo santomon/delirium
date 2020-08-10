232a8ea8d636d5e04dab751223809f48c19ba085
santomon <qa12_8@yahoo.de>
2020-08-10 15:23:18 +0000

    template for generic inference function

--- utility.py
+++ utility.py
@@ -1,2 +1,27 @@
+import os
 
 BOLD5KSUBDIRS = ["COCO", "ImageNet", "Scene"]
+FENDINGS = ["jpg", "JPEG", "JPG"]
+
+def infer_recursive():
+  """
+    infers all
+  """
+  pass
+
+
+
+def infer_folder(
+  img_dir,
+  out_dir,
+  fendings = FENDINGS,
+  model_ = None,
+  model_params = [],
+  compressor = None,
+  compress_params = [],
+):
+  """
+    given a list of file endings; applies the model for all those files
+    in a specified directory
+  """
+  pass
