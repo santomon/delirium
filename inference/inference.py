8d454ed73680505031535b56c9060d304b3a2705
santomon <qa12_8@yahoo,de>
2020-09-05 20:21:54 +0200

    added argparser, more TODO, added try except for BOLD5K decorator

--- inference/inference.py
+++ inference/inference.py
@@ -3,8 +3,7 @@
 TODO: doc, testing
 """
 
-
-
+import argparse
 import typing as t
 import tqdm
 import torch
@@ -12,6 +11,7 @@ import tensorflow as tf
 import os
 
 import sys
+
 sys.path.append(".")
 
 import delirium_config
@@ -21,7 +21,7 @@ Tensor = t.Union[torch.Tensor, tf.Tensor]
 Type_of_Data = t.Any
 
 
-#decorator function to use
+# decorator function to use
 def infer_BOLD5K(subdirectories=delirium_config.BOLD5K_PRES_STIM_SUBDIRECTORIES):
     """
 
@@ -30,18 +30,22 @@ def infer_BOLD5K(subdirectories=delirium_config.BOLD5K_PRES_STIM_SUBDIRECTORIES)
     """
 
     def inner(infer_function):
-
         def wrapper(*args, **kwargs):
-
             parent_path = args[0]
             args_ = list(args)
             for subdirectory in subdirectories:
-
-                args_[0] = os.path.join(parent_path, subdirectory)
+                try:
+                    args_[0] = os.path.join(parent_path, subdirectory)
+                except:
+                    try:
+                        kwargs['data_path'] = os.path.join(parent_path, subdirectory)
+                    except:
+                        pass
                 infer_function(*args_, **kwargs)
-        return wrapper
-    return inner
 
+        return wrapper
+
+    return inner
 
 
 @infer_BOLD5K(subdirectories=delirium_config.BOLD5K_PRES_STIM_SUBDIRECTORIES)
@@ -53,9 +57,11 @@ def infer_folder(
         mode_call: t.Callable[[Tensor], t.Any],
         postprocessor: t.Callable[[t.Any], t.Any],
         saver: t.Callable[[t.Any, str, str], t.NoReturn],
-) -> t.NoReturn:
+) -> t.Any:
     """
     TODO:doc
+        "upgrade", to incorporate a namegenerator; would be helpful to check, if file should be skipped
+
     :param data_path:
     :param save_path:
     :param loader:
@@ -78,12 +84,32 @@ def infer_folder(
 
 
 
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_module", default="deeplabv3_inference", type=str,
+                        help="will attempt to import model_module"
+                             "any model_module has to implement methods:"
+                             "      data_path: str,"
+                             "     save_path: str,"
+                             "       loader: t.Callable[[str], Type_of_Data],"
+                             "       preprocessor: t.Callable[[Type_of_Data], Tensor],"
+                             "       mode_call: t.Callable[[Tensor], t.Any],"
+                             "      postprocessor: t.Callable[[t.Any], t.Any],"
+                             "       saver: t.Callable[[t.Any, str, str], t.NoReturn],"
+                             "specify a file which implements method, to run network inference on a set of data"
+                             "depending on the implementation of the model by the original authors, this can vary from"
+                             "easy to rather complicated;"
+                             )
+
+    return parser.parse_args()
 
 
+    pass
 
 if __name__ == "__main__":
 
-
+    parser = parse_args()
 
     infer_folder(
         os.path.join(delirium_config.BOLD5K_STIMULI_PATH, delirium_config.BOLD5K_PRES_STIM_SUBPATH),
