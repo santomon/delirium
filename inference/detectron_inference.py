a84acce6e52a2a2adf3955338cc9ad7f02b7dab3
lmao <xd>
2020-11-23 16:59:54 +0100

    predictor class prototype

--- inference/detectron_inference.py
+++ inference/detectron_inference.py
@@ -11,32 +11,34 @@ from detectron2.detectron2.layers import ShapeSpec
 from detectron2.detectron2.data import MetadataCatalog
 from detectron2.detectron2.checkpoint import DetectionCheckpointer
 from detectron2.detectron2.config import get_cfg
+import detectron2.detectron2.data.transforms as T
 
 from detectron2.detectron2.model_zoo import model_zoo
 
 
-available_models = model_zoo._ModelZooUrls.CONFIG_PATH_TO_URL_SUFFIX.keys()
+viable_models = model_zoo._ModelZooUrls.CONFIG_PATH_TO_URL_SUFFIX.keys()
 # the viable model names can be found there
+# https://github.com/facebookresearch/detectron2/blob/master/detectron2/model_zoo/model_zoo.py  # last checked 23.11.20
+
+
+
+default_model = "COCO-Detection/retinanet_R_50_FPN_3x.yaml"
+
+
 
 device: torch.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 cfg = get_cfg()
 
 
-class FeatureGetter:
+class FeatureExtractor:
     """
-    --- straight up copy pasta from detectron2 code of DefaultPredictor,
-    modified, so that the backbone output can be safely infered---
-
-    call(input_img : opened by cv2.imread(), pyramid: str) --> 4-dim tensor representing the output of the selected pyramid;
-    valid values for pyramid:'p2' 'p3' 'p4' 'p5' 'p6'
-
 
     Create a simple end-to-end predictor with the given config that runs on
     single device for a single input image.
     Compared to using the model directly, this class does the following additions:
     1. Load checkpoint from `cfg.MODEL.WEIGHTS`.
-    2. Always take BGR image as the input and apply conversion defined by `cfg.INPUT.FORMAT`.
+    2. Always take RGB image as the input and apply conversion defined by `model.input_format`.
     3. Apply resizing defined by `cfg.INPUT.{MIN,MAX}_SIZE_TEST`.
     4. Take one input image and produce a single output, instead of a batch.
     If you'd like to do anything more fancy, please refer to its source code
@@ -51,28 +53,15 @@ class FeatureGetter:
         outputs = pred(inputs)
     """
 
-    def __init__(self, cfg):
-        self.cfg = cfg.clone()  # cfg can be modified by model
-        self.model = build_model(self.cfg)
+    def __init__(self, cfg_path: str):
 
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        self.model.to(device)
-
-        self.default_input_shape = ShapeSpec(channels=len(cfg.MODEL.PIXEL_MEAN))
-        self.model.eval()
-        self.metadata = MetadataCatalog.get(cfg.DATASETS.TEST[0])
-
-        checkpointer = DetectionCheckpointer(self.model)
-        checkpointer.load(cfg.MODEL.WEIGHTS)
-
+        self.cfg  = model_zoo.get_config(cfg_path)
+        self.model = model_zoo.get(cfg_path)
         self.aug = T.ResizeShortestEdge(
             [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
         )
 
-        self.input_format = cfg.INPUT.FORMAT
-        assert self.input_format in ["RGB", "BGR"], self.input_format
-
-    def __call__(self, original_image, pyramid: str):
+    def __call__(self, original_image):
         """
         Args:
             original_image (np.ndarray): an image of shape (H, W, C) (in BGR order).
@@ -83,7 +72,7 @@ class FeatureGetter:
         """
         with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
             # Apply pre-processing to image.
-            if self.input_format == "RGB":
+            if self.model.input_format == "BGR":
                 # whether the model expects BGR inputs or RGB
                 original_image = original_image[:, :, ::-1]
             height, width = original_image.shape[:2]
@@ -100,6 +89,10 @@ class FeatureGetter:
             return predictions
 
 
+predictor: FeatureExtractor = FeatureExtractor(default_model)
+
+
+
 def dependency_solver(): #implement this, if needed
     pass
 
@@ -107,8 +100,7 @@ def dependency_solver(): #implement this, if needed
 def loader(path_to_image: str) -> np.ndarray:
 
     img = np.asarray(Image.open(path_to_image))  # loaded as RGB
-    return img[:,:,::-1]    # reverse color order; essentially RGB -> BGR, which is required for detectron2 models
-                            # what's funny is, is that the preprocessing step of detectron2 converts it back to RGB...
+    return img
 
 
 def preprocessor(data_):
@@ -125,3 +117,38 @@ def postprpocessor(data_):
 
 def saver(data_, save_path, name):
     pass
+
+
+def select_model(model_name: str):
+    """
+    change the model, that will be used by the predictor;
+    to get the model_names, refer to viable models at the top of this file
+    """
+    global predictor
+    predictor = FeatureExtractor(model_name)
+
+
+def get_features_by_image_path(path_to_image: str) -> t.Dict[str, torch.Tensor]:
+    """
+    API function for Algonauts; for a given string, that is a path to an image file, compute a dictionary
+    of of outputs of various layers from the encoding model
+    """
+    image_data = loader(path_to_image)
+    with torch.no_grad():
+        return get_features_by_image_data(image_data)
+
+
+
+def get_features_by_image_data(image_data: np.ndarray) -> t.Dict[str, torch.Tensor]:
+    """
+
+    API function for Algonauts; for a loaded, not yet preprocessed image,
+    return outputs of various layers of the encoding model;
+    image data is expected to be in RGB
+
+    the layers here are provided by detectron2 itself;
+    no IntermediateLayerGetter
+    """
+    with torch.no_grad():
+        return predictor(image_data)
+    
