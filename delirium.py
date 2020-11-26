ab8b764bd8e317371b17e4378891bd3854d636df
lmao <xd>
2020-11-26 17:49:34 +0100

    permutation tests?

--- delirium.py
+++ delirium.py
@@ -12,10 +12,14 @@ import scipy.io
 import numpy as np
 import sys
 import importlib
+import pickle
+
+from sklearn.decomposition import PCA
 
 import utility
 import delirium_config as config
 import tqdm
+from NeuralTaskonomy.code.encodingmodel.encoding_model import ridge_cv
 
 sys.path.append(os.path.join(config.NT_PATH, "code"))
 
@@ -66,6 +70,8 @@ def load_brain_data(
         raise TypeError("tr should be either instance of int or list[int]!")
 
 
+
+
 def load_stim_lists(brain_data_path: str = config.BOLD5K_ROI_DATA_PATH, subjects: t.List[int] = (1, 2, 3)) -> \
     t.List[t.List[str]]:
     stim_lists = []
@@ -109,24 +115,22 @@ def eliminate_from_data_by_substr(data_: brain_dtype, stim_lists: t.List[t.List[
 
 def load_nn_data(
         stim_list: t.List[str],
-        nn_data_path: str,
+        full_nn_data_path: str,
+        module_name: str,
 ) -> np.ndarray:
     """
     TODO:doc
     :param stim_list:
-    :param nn_data_path:
+    :param nn_data_path: path to where the files are located, currently only supports models, generated from inference/inference.py
     :return:
     """
-
-    rest_path, model_name = os.path.split(nn_data_path)
-    _, module_name = os.path.split(rest_path)
 
     #VULNERABLE:
     module = importlib.import_module("inference." + module_name)
 
     data_: t.List[np.ndarray] = []
     for img_name in tqdm.tqdm(stim_list):
-        data_path = os.path.join(nn_data_path, module.generate_file_name(img_name))
+        data_path = os.path.join(full_nn_data_path, module.generate_file_name(img_name))
         data_.append(np.load(data_path, allow_pickle=True).flatten())
     data_: np.ndarray = np.array(data_)
     assert len(data_.shape) == 2, "Error: not all datapoints have the same number of parameters!"
@@ -158,9 +162,99 @@ def rearrange_nn_data(nn_data: np.ndarray,
     return nn_data[idx_tmp]
 
 
-def permutation_test():
-    pass
+def permutation_test_SSF(
+        n: int,
+        data_path :str,
+        module_name: str,
+        model_name: str,
+        save_path: str,
+        do_pca: bool,
+        BOLD5000_ROI_path: str,
+        BOLD5000_Stimuli_path: str,
+        subjects: t.List[int] = (1, 2, 3),
+        TR: t.List[int] = (3, 4)
 
+):
+    brain_data = [load_brain_data(subject=i, tr=TR) for i in subjects]
+    stim_lists = load_stim_lists(subjects=subjects)
+
+    brain_data, stim_lists = eliminate_from_data_by_substr(
+        brain_data,
+        stim_lists,
+        substr='rep_'
+    )
+
+    for substr in config.UNWANTED_IMAGES:
+        brain_data, stim_lists = eliminate_from_data_by_substr(brain_data, stim_lists, substr)
+
+    utility.inspect(brain_data)
+
+    fit_encoding_model_SSF(
+        brain_data = brain_data,
+        stim_lists = stim_lists,
+        subjects = subjects,
+        nn_data_path = data_path,
+        module_name= module_name,
+        model_name = model_name,
+        save_path=save_path,
+        do_pca=do_pca,
+        repeat=n,
+        permute_y=True,
+        do_cv=False,
+    )
+
+
+
+
+def fit_encoding_model_SSF(
+        brain_data,
+        stim_lists,
+        subjects: t.List[int],
+        nn_data_path: str,
+        module_name: str,
+        model_name: str,
+        save_path: str,
+        do_pca: bool,
+        repeat=1,
+        permute_y = False,
+        do_cv = False,
+):
+    ridged_brain_data = []
+    for i, brain_data_single in zip(subjects, brain_data):
+
+        """
+        if i==1:
+          data = nn_data
+        else:
+          data = delirium.rearrange_nn_data(nn_data, 1, i, stim_lists)
+        """
+        data = load_nn_data(
+            stim_list=stim_lists[i - 1],
+            full_nn_data_path=nn_data_path,
+            module_name=module_name
+        )
+
+        ridged_brain_data_single = dict()
+        for roi in config.ROI_LABELS:
+            corrs, *cv_outputs =ridge_cv(X=np.float32(data),
+                                            y=np.float32(brain_data_single[roi]),
+                                            permute_y= permute_y,
+                                            cv=do_cv,
+                                            pca = do_pca,
+                                            split_by_runs=False,
+                                            repeat=repeat)
+
+            if permute_y:
+                full_save_path = os.path.join(save_path, module_name, model_name, 'permutations')
+                np.save(os.path.join(full_save_path, 'subj{}_permutation_test_on_test_data_corr.npy'.format(i)), 'wb')
+
+                pickle.dump(
+                    cv_outputs[0],
+                    open(
+                        os.path.join(full_save_path, "subj{}permutation_test_on_test_data_pvalue.p".format(i)),
+                        "wb",
+                    ),
+                )
 
 
 
--- main.py
+++ main.py
@@ -22,7 +22,11 @@ def parse_args() -> argparse.Namespace:
                          help="choose a model for which to run an experiment; "
                               " the string will be treated as a subfolder of the data_path;\n\n"
                               "the keyword 'all' can be used instead, to run a given experiment on all subfolders in "
-                              "data_path, that can be acquired with os.listdir")
+                              "<data_path>/<module>/..., that can be acquired with os.listdir")
+
+    _parser.add_argument("--module", default="all", type=str,
+                         help="select the module, which the model was part of. if 'all' keyword is used,"
+                              " will try to run the experiment on all subfolders of data_path")
 
     _parser.add_argument("--data_path", default=delirium_config.NN_DATA_PATH, type=str,
                          help="base path for, where the features are stored; defaults to delirium_config.NN_DATA_PATH,"
@@ -33,13 +37,18 @@ def parse_args() -> argparse.Namespace:
     _parser.add_argument("--do_permutations", default=0, type=int,
                          help="if selected, the the regression model will be done the specified number of times "
                               "with randomised train-test-splits; and stored in: \n"
-                              "<path>/<model>/permutations")
+                              "<save_path>/<module>/<model>/permutations")
 
     _parser.add_argument("--do_pca", action="store_true",
                          help="specify, if PCA should be applied on the data, before running a given experiment;\n"
                               "this is highly recommended, as feature spaces can be really large and potentially cause"
                               "your RAM to explode")
-    _parser.add_argument()
+    _parser.add_argument("--BOLD5000_ROI_path", default=os.path.join(delirium_config.BOLD5K_ROI_DATA_PATH), type=str,
+                         help="specify, where to find the BOLD5000 ROI dataset")
+
+    _parser.add_argument("--BOLD5000_Stimuli_path", default=os.path.join(delirium_config.BOLD5K_STIMULI_PATH, delirium_config.BOLD5K_PRES_STIM_SUBPATH),
+                         type=str,
+                         help="specify, where to find the presented stimuli from the BOLD5000 experiment")
 
 
     if 'main.py' in sys.argv:
@@ -47,17 +56,35 @@ def parse_args() -> argparse.Namespace:
     else:
         return _parser.parse_args("")
 
+
 def main():
     args: argparse.Namespace = parse_args()
 
-    if args.model == 'all':
-        models: t.List[str] = [file for file in os.listdir(args.data_path) if os.path.isdir(os.path.join(args.data_path, file))]
+    if args.module == 'all':
+        module_names = os.listdir(args.data_path)
     else:
-        models: t.List[str] = [os.path.join(args.data_path, args.model)]
+        module_names = [args.module]
 
-    for model in models:
-        if args.do_permutations > 0:
-            delirium.do
+    for module_name in module_names:
+        if args.model == 'all':
+            model_names: t.List[str] = [file for file in os.listdir(args.data_path) if os.path.isdir(os.path.join(args.data_path, module_name, file))]
+        else:
+            model_names: t.List[str] = [args.model] if os.path.isdir(os.path.join(args.data_path, module_name, args.model)) else []
+
+        if model_names == []:
+            print("model(s) could not be found!")
+
+        for model_name in model_names:
+            if args.do_permutations > 0:
+                delirium.permutation_test_SSF( n = args.do_permutations,
+                                           data_path = args.data_path,
+                                           module_name = module_name,
+                                           model_name = model_name,
+                                           save_path = args.save_path,
+                                           do_pca = args.do_pca,
+                                           BOLD5000_ROI_path = args.BOLD5000_ROI_path,
+                                           BOLD5000_Stimuli_path = args.BOLD5000_Stimuli_path,
+                )
 
 
 
