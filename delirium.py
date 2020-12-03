removed data leaking, added fix testing option

delirium.py
@@ -216,6 +216,7 @@ def fit_encoding_model_SSF(
         model_name: str,
         save_path: str,
         do_pca: bool,
+        fix_testing,
         repeat=1,
         permute_y = False,
         do_cv = False,
@@ -235,18 +236,14 @@ def fit_encoding_model_SSF(
             module_name=module_name
         )
 
-        if do_pca:
-            pca = PCA()
-            pca.fit(data[:1000])
-            data = pca.transform(data)
-
         ridged_brain_data_single = dict()
         for roi in config.ROI_LABELS:
             corrs, *cv_outputs =ridge_cv(X=np.float32(data),
                                             y=np.float32(brain_data_single[roi]),
                                             permute_y= permute_y,
                                             cv=do_cv,
-                                            pca = False,
+                                            pca = do_pca,
+                                            fix_testing= fix_testing,
                                             split_by_runs=False,
                                             repeat=repeat)
 