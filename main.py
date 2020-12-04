b011b2800710d4e6e1485b00583164b44dd28092
lmao <xd>
2020-12-04 18:43:51 +0100

    main delirium

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def main():
             encodingmodel = delirium.EncodingModel(
                                                     data_path=args.data_path,
                                                     module_name= args.module,
-                                                    model_name= args.model_name,
+                                                    model_name= args.model,
                                                     save_path=args.save_path,
                                                     do_pca=args.do_pca,
                                                     fix_testing=args.fix_testing,
