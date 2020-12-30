dd3c8e6a5d120a1f1ed00e1735c585a120f5b1a6
lmao <xd>
2020-12-30 17:08:24 +0100

    permutation

--- permutation.py
+++ permutation.py
@@ -5,9 +5,10 @@ import pickle
 
 import pandas as pd
 import numpy as np
+from scipy.stats import pearsonr
 
 import delirium_config
-
+from NeuralTaskonomy.code.util.util import pearson_corr, empirical_p
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -22,8 +23,13 @@ class Permutator():
         self.data = pd.DataFrame(columns=["module_name", "model_name", "subj", "yhat", "ylabel", "ROI", "Hemisphere", "did_pca"])
 
 
+    def permute(self):
+        grouped = self.data.groupby(list(set(self.data.columns) - set(["yhat", "ylabel"])), axis=1)
+        grouped.apply(_permute_single, self.repeats)
+
+
     def load_predictions(self, module_name, model_name: str, did_pca: bool, fixed_testing: bool, did_cv: bool, TR: t.List,
-                         result_path: str=delirium_config.NN_RESULT_PATH, *fname_spec):
+                         result_path: str = delirium_config.NN_RESULT_PATH, *fname_spec):
         [self._load_prediction(subj, module_name, model_name, did_pca, fixed_testing, did_cv, TR, result_path, *fname_spec)
          for subj in range(1, 4)]
 
@@ -51,23 +57,43 @@ class Permutator():
 
     def _append_data(self, _data, module_name, model_name, subj, did_pca):
         for i, (roi, roi_data) in enumerate(zip(delirium_config.ROI_LABELS, _data)):
-            
+
             _len = len(roi_data[0])
             _data_dict = {
-                "module_name": [module_name for i in range(_len)],
-                "model_name": [model_name for i in range(_len)],
-                "subj": [subj for i in range(_len)],
+                "module_name": [module_name for _ in range(_len)],
+                "model_name": [model_name for _ in range(_len)],
+                "subj": [subj for _ in range(_len)],
                 "yhat": [x for x in roi_data[0]],
                 "ylabel": [x for x in roi_data[1]],
                 "ROI": [roi[2:] for i in range(_len)],
-                "Hemisphere": [roi[:2] for i in range(_len)],
-                "did_pca": [did_pca for i in range(_len)]
+                "Hemisphere": [roi[:2] for _ in range(_len)],
+                "did_pca": [did_pca for _ in range(_len)]
             }
 
             rdata = pd.DataFrame(_data_dict)
             self.data = pd.concat([self.data, rdata])
 
 
+
+def _permute_single(data: pd.DataFrame, repeats: int):
+    # expected keys are: "yhat", "ylabel"
+
+    yhat: pd.DataFrame = data["yhat"]
+    ylabel: pd.DataFrame = data["ylabel"]
+    corrs_dist = []
+    original_corrs = [pearsonr(ylabel[:, i], yhat[:, i]) for i in range(ylabel.shape[1])]
+
+    label_idx = np.arange(ylabel.shape[0])
+    for _ in range(repeats):
+        np.random.shuffle(label_idx)
+        y_test_perm = ylabel[label_idx, :]
+        perm_corrs = pearson_corr(y_test_perm, yhat, rowvar=False)
+        corrs_dist.append(perm_corrs)
+    p = empirical_p(original_corrs, np.array(corrs_dist))
+
+    assert len(p) == ylabel.shape[1]
+
+    return pd.Series([p], index=["empirical_ps"])
 
 
 def main():
