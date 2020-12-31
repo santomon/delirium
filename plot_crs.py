4a3fe0eacfec6e655aad6b2c074f2ff7accb2ead
lmao <xd>
2020-12-31 17:59:18 +0100

    permutation

--- plot_crs.py
+++ plot_crs.py
@@ -70,6 +70,7 @@ class Plotter:
             handles=handles,
             labels=legend_labels if "legend_labels" not in kwargs.keys() else kwargs['legend_labels'],
             loc="upper center",
+            ncol=5,
         )
 
         plt.show()
