3bf6fe02755c86349e8434e06db7e4ae45b37b8c
lmao <xd>
2021-01-05 18:34:41 +0100

    permutation

--- permutation.py
+++ permutation.py
@@ -117,8 +117,8 @@ def _permute_single(data: pd.DataFrame, repeats: int, save_permutations, save_di
         full_path = os.path.join(save_dir_root, data.name[0], data.name[1], "permutation_results")
         if not os.path.isdir(full_path):
             os.makedirs(full_path)
-        permutation_file_name = utility.generate_permutation_file_name(*data.name[2:])
-        pvalues_file_name = utility.generate_pvalues_file_name(*data.name[:2])
+        permutation_file_name = utility.generate_permutation_file_name(*data.name[2:-1], *data.name[-1])
+        pvalues_file_name = utility.generate_pvalues_file_name(*data.name[2:-1], *data.name[-1])
 
         pickle.dump(corrs_dist, open(os.path.join(full_path, permutation_file_name), "wb"))
         pickle.dump(corrs_dist, open(os.path.join(full_path, pvalues_file_name), "wb"))
